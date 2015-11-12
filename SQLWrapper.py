d9d17f74722c1ef29738990d85b56e333c5c32ca
Benjamin <fellfromatree@gmail.com>
2015-11-12 03:25:45 +0000

    some test sql code and the flask code

--- SQLWrapper.py
+++ SQLWrapper.py
@@ -1,21 +1,24 @@
 class SQLWrapper:
     #I dunno what you'll need for your constructor
-    def _init_(this, fill your stuff):
+    def _init_(this, fill_your_stuff):
         pass
     #Check the username and password, then generate the logintoken
+    #The password passed is the hashed uesr password. The password stored should be that hashed.
     def login(this, username, password):
         return logintoken
+    def checkToken(this,token):
+        return bool
     #Delete the Logintoken
     def logout(this, logintoken):
         pass
     #check out an object. Pretty self-explanitory.
     def checkOut(this, objId, toId, percent, loginToken):
         pass
-    #Return a list of all the objects represting anything with a lazy string complete (I'll get you some algorithm docs/libraries later)
+    #Return a list of all the objects containing this string
     def searchByName(this, name):
-        return results 
+        return results
     #Return the info of an object with this ID
-    def getInfoById(this, objId}:
+    def getInfoById(this, objId):
         return result
     #Return all history, going back num transactions (-1 for all)
     def getHistory(this, num):
@@ -27,9 +30,58 @@ class SQLWrapper:
     def getHistoryByCheckoutLocation(this, locId, num):
         return history
     #Return all the history for a given item, going back num transactiosn (-1 for all)
-    def getHistoryByIten(this, itemId, num):
+    def getHistoryByIteM(this, itemId, num):
         return history
-
-class InvalidLoginException(Exceptino):
+    #return a listing of all users and their names
+    def allUsers(this):
+        return allUser
+    #info for one user
+    def userInfo(this, user):
+        return user
+        
+class InvalidLoginException(Exception):
     #yeaaaaah
     pass
+class InvalidIdException(Exception):
+    pass
+
+class DummySQLWrapper:
+    #I dunno what you'll need for your constructor
+    def _init_(this):
+        pass
+    #Check the username and password, then generate the logintoken
+    #The password passed is the hashed uesr password. The password stored should be that hashed.
+    def login(this, username, password):
+        return {username:password}
+    def checkToken(this,token):
+        return True
+    #Delete the Logintoken
+    def logout(this, logintoken):
+        pass
+    #check out an object. Pretty self-explanitory.
+    def checkOut(this, objId, toId, percent, loginToken):
+        pass
+    #Return a list of all the objects containing this string
+    def searchByName(this, name):
+        return {"ayyyy" : name}
+    #Return the info of an object with this ID
+    def getInfoById(this, objId):
+        return {"blazeit" : objId}
+    #Return all history, going back num transactions (-1 for all)
+    def getHistory(this, num):
+        return {"Black" : num}
+    #Get all the history from a given user, going back num transactions (-1 for all)
+    def getHistoryByUser(this, userId, num):
+        return {"Becker" : str(userId) +"APWH"+ str(num)}
+    #Return all the history for a given checkout Location, going back num transactions(-1 for all)
+    def getHistoryByCheckoutLocation(this, locId, num):
+        return {"Olivera" : str(locId) + "APUSH" + str(num)}
+    #Return all the history for a given item, going back num transactiosn (-1 for all)
+    def getHistoryByIteM(this, itemId, num):
+        return {"Now the party dont start": str(itemId) + "Till I walk in" + str(num)}
+    #return a listing of all users and their names
+    def allUsers(this):
+        return {"Everybody dance now" : "Everybody, rock your body"}
+    #info for one user
+    def userInfo(this, user):
+        return {"dun, dun, dun dun dun " : user }
---
+++ main.py
@@ -0,0 +1,77 @@
+from flask import Flask, request, redirect
+import os
+from SQLWrapper import sql
+import DummySQLWrapper
+import json
+from flask.ext.api.exceptions import APIException, AuthenticationFailed, ParseError
+
+app = Flask(__name__)
+sql = DummySQLWrapper()
+ 
+@app.route("/test", methods=['GET', 'POST'])
+def hello_monkey():
+    return "ayyyy it works"
+@app.route("/login", methods=['GET', 'POST'])
+def login():
+    uname = request.args.get("username")
+    password = request.args.get("password")
+    try:
+        return sql.login(uname, password)
+    except:
+        raise AuthenticationFailed()
+    return "what"
+@app.route("/logout", methods=['POST'])
+def logout():
+    sql.logout(request.args.get("token"))
+@app.route("/checkOut", methods=["POST"])
+def checkOut():
+    objId = request.args.get("objId")
+    toId = request.args.get("toId")
+    percent = request.args.get("percent")
+    loginToken = request.args.get("loginToken")
+    sql.checkOut(objId, toId, percent, loginToken)
+@app.route("/search/<name>", methods=["GET", "POST"])
+def searchByName(name):
+    return json.dumps(sql.searchByName(name))
+@app.route("/item/<name>", methods=["GET", "POST"])
+def search(name):
+    return json.dumps(sql.getInfoById(name))
+@app.route("/history", methods=["GET", "POST"])
+def getHistory():
+    num = request.args.get("number")
+    if num is None:
+        num = -1
+    return json.dumps(sql.getHistory(num))
+@app.route("history/obj/<name>", methods=["GET", "POST"])
+def itemHistory(name):
+    num = request.args.get("number")
+    if num is None:
+        num = -1
+    return json.dumps(sql.getHistoryByIteM(name,num))
+@app.route("history/user/<name>", methods=["GET", "POST"])
+def userHistory(name): 
+    num = request.args.get("number")
+    if num is None:
+        num = -1
+    return json.dumps(sql.getHistoryByUser(name,num))
+@app.route("history/location/<name>", methods=["GET", "POST"])
+def locHistory(name):
+    num = request.args.get("number")
+    if num is None:
+        num = -1
+    return json.dumps(sql.getHistoryByCheckoutLocation(name,num))
+@app.route("user/list", methods=["POST"])
+def getUsers():
+    if not sql.checkToken(request.args.get("token")):
+        return "waaaat"
+    return json.dumps(sql.allUsers())
+@app.route("user/info/<user>", methods=["POST"])
+def getUser(user):
+    if not sql.checkToken(request.args.get("token")):
+        return "waaaat"
+    return json.dumps(sql.userInfo(user))
+
+if __name__ == "__main__":
+    app.run("0.0.0.0",
+            8080,
+            debug=True)
